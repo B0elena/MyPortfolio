00dbc49368b7e9d7837132c27348acf1cb22915a
B0elena <e.mc2.1012md@gmail.com>
2020-11-30 00:56:29 +0900

    要素をふわっと表示

--- index.js
+++ index.js
@@ -33,6 +33,20 @@ $(function() {
   });
 });
 
+// フェードイン
+$(function(){
+  $(window).scroll(function (){
+    $('.effect-fade').each(function(){
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight){
+        $(this).addClass('effect-scroll');
+      }
+    });
+  });
+});
+// フェードイン
 
 $(function(){
   $(window).scroll(function (){
